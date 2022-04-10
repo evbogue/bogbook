65780dffbe871630b2d034443e9093e680fc4c06
Ev Bogue <ev@evbogue.com>
2022-04-10 09:45:05 -0500

    navbar tweaks

--- navbar.js
+++ navbar.js
@@ -1,10 +1,15 @@
 import { keys } from './browserkeys.js'
+import { getImage, getName } from './avatar.js'
 
 export function navbar (screen) {
   const navbar = h('div', {id: 'navbar'})
   document.body.appendChild(navbar)
   const internal = h('div', {classList: 'internal'}, [
-    h('a', {href: '#' + keys.pubkey()}, [keys.pubkey().substring(0, 7)]),
+    h('a', {href: '#' + keys.pubkey()}, [
+      getImage(keys.pubkey()), 
+      ' ',
+      getName(keys.pubkey())
+    ]),
     ' ',
     h('a', {href: '#'}, ['Home'])
   ])
