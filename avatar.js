b43b0ece7376c8c6d42682ae2e3742d8b1e78b7b
Ev Bogue <ev@evbogue.com>
2022-04-18 08:28:02 -0500

    cache names

--- avatar.js
+++ avatar.js
@@ -1,5 +1,11 @@
 import { h, vb } from './lib/misc.js'
 import { decode } from './lib/base64.js'
+import { keys } from './browserkeys.js'
+import { publish, open } from './sbog.js'
+import { render } from './render.js'
+import { logs } from './browserlog.js'
+
+const kv = new IdbKvStore('ssboat')
 
 export function getImage (id) {
   let img = vb(decode(id), 256)
@@ -8,7 +14,17 @@ export function getImage (id) {
 }
 
 export function getName (id) {
-  return id.substring(0, 10) + '...'
+  const nameDiv = h('span')
+  nameDiv.textContent = id.substring(0, 10) + '...'
+  logs.getLog().then(log => {
+    for (let i = 0; i < log.length; i++) {
+      if (log[i].named === id) {
+        nameDiv.textContent = log[i].name
+        kv.set('name:' + id, log[i].name)
+      }
+    }
+  })
+  return nameDiv
 }
 
 export function getBoth (id) {
@@ -18,4 +34,33 @@ export function getBoth (id) {
     getName(id)
   ])
   return both
+}
+
+export function newName (id, div) {
+  const input = h('input', {placeholder: 'Give a name to ' + id.substring(0, 10)})
+
+  if (id === keys.pubkey()) {
+    input.placeholder = 'Give yourself a name.'
+  }
+
+  const nameDiv = h('div', [
+    input,
+    h('button', {onclick: function () {
+      if (input.value.length === keys.keypair().length) {
+        alert('Error: Names cannot be the same length as your keypair!')
+      }
+      if (input.value) {
+        const obj = {name: input.value, named: id}
+        publish(obj).then(msg => {
+          open(msg).then(opened => {
+            render(opened).then(rendered => {
+              div.appendChild(rendered)
+            })
+          })
+        })
+      } 
+    }},['Publish'])
+  ])
+
+  return nameDiv
 }
