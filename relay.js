10a8edfc3ff8c4815a855681f252099181b20953
Ev Bogue <ev@evbogue.com>
2022-06-26 19:43:48 -0500

    try using a simple relay on deno deploy

---
+++ relay.js
@@ -0,0 +1,25 @@
+import { listenAndServe } from "https://deno.land/std/http/server.ts"
+import { serveDir } from "https://deno.land/std@0.144.0/http/file_server.ts"
+
+const sockets = new Set()
+
+const channel = new BroadcastChannel("")
+
+channel.onmessage = e => {
+  console.log(e.data)
+  (e.target != channel) && channel.postMessage(e.data)
+  sockets.forEach(s => s.send(e.data))
+}
+
+await listenAndServe(":8080", (r) => {
+  try {
+    const { socket, response } = Deno.upgradeWebSocket(r)
+    sockets.add(socket)
+    socket.onmessage = channel.onmessage
+    socket.onclose = _ => sockets.delete(socket)
+    return response
+  } catch {
+    return serveDir(r, {fsRoot: '', showDirListing: true, quiet: true})
+  }
+})
+
