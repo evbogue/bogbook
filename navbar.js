a98558d5f4044e26f210c711d2b4978c3265a69a
Ev Bogue <ev@evbogue.com>
2022-07-04 08:59:03 -0500

    activate bootstrap navbar buttons on routes

--- navbar.js
+++ navbar.js
@@ -3,15 +3,34 @@ import { keys } from './browserkeys.js'
 import { getBoth } from './avatar.js'
 
 export function navbar (screen) {
+
+  const src = window.location.hash.substring(1)
+
+  const profile = h('li', [getBoth(keys.pubkey())])
+  const homepage = h('li', [h('a', {href: '#'}, ['Home'])])
+  const keypage = h('li', [h('a', {href: '#key'}, ['Key'])])
+
+  if (src === '') {
+    homepage.classList = 'active'
+  } 
+
+  if (src === 'key') {
+    keypage.classList = 'active'
+  }
+
+  if (src === keys.pubkey()) {
+    profile.classList = 'active'
+  } 
+
   const navbar = h('div', {classList: 'navbar navbar-fixed-top'})
   const internal = h('div', {classList: 'navbar-inner'}, [
     h('ul', {classList: 'nav'}, [
-
-      h('li', [getBoth(keys.pubkey())]),
-      h('li', [h('a', {href: '#'}, ['Home'])]),
-      h('li', [h('a', {href: '#key'}, ['Key'])])
+      profile,
+      homepage,
+      keypage
     ])
   ])
+
   navbar.appendChild(internal)
 
   return navbar
