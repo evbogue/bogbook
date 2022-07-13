0c0bb5859e006a8be78d100cbbc87020afb8d65f
Ev Bogue <ev@evbogue.com>
2022-07-13 11:30:41 -0500

    sort on an interval instead of on route change

--- browserlog.js
+++ browserlog.js
@@ -21,7 +21,7 @@ kv.get('log', function (err, file) {
         }
       })
     })
-    //arraystore.sort((a,b) => a.timestamp - b.timestamp)
+    arraystore.sort((a,b) => a.timestamp - b.timestamp)
   }
 })
 
@@ -29,7 +29,7 @@ let newData = true
 
 setInterval(function () {
   if (newData) {
-    //arraystore.sort((a,b) => a.timestamp - b.timestamp)
+    arraystore.sort((a,b) => a.timestamp - b.timestamp)
     kv.set('log', log)
     newData = false
   }
@@ -41,7 +41,7 @@ export const logs = function logs (query) {
       if (arraystore[0]) {
         const querylog = arraystore.filter(msg => msg.author == query)
         if (querylog[0]) {
-          querylog.sort((a,b) => a.timestamp - b.timestamp)
+          //querylog.sort((a,b) => a.timestamp - b.timestamp)
           return querylog[querylog.length - 1]
         }
       } 
@@ -50,7 +50,7 @@ export const logs = function logs (query) {
       if (arraystore[0]) {
         const querylog = arraystore.filter(msg => msg.author == query)
         if (querylog[0]) {
-          querylog.sort((a,b) => a.timestamp - b.timestamp)
+          //querylog.sort((a,b) => a.timestamp - b.timestamp)
           return querylog[querylog.length - 1].hash
         }
       } 
@@ -67,18 +67,18 @@ export const logs = function logs (query) {
       return feeds
     },
     getLog: async function () {
-      arraystore.sort((a,b) => a.timestamp - b.timestamp)
+      //arraystore.sort((a,b) => a.timestamp - b.timestamp)
       return arraystore
     },
     query: async function (query) {
       if (arraystore[0]) {
         if (query.startsWith('?')) {
           const querylog = arraystore.filter(msg => msg.text && msg.text.includes(query.substring(1)))
-          querylog.sort((a,b) => a.timestamp - b.timestamp)
+          //querylog.sort((a,b) => a.timestamp - b.timestamp)
           return querylog 
         } else {
           const querylog = arraystore.filter(msg => msg.author == query || msg.hash == query)
-          querylog.sort((a,b) => a.timestamp - b.timestamp)
+          //querylog.sort((a,b) => a.timestamp - b.timestamp)
           return querylog 
         }
       }
