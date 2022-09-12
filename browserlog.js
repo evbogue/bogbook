59c6b71d4ce261092f40a8b1965014fe98c1f653
Ev Bogue <ev@evbogue.com>
2022-09-12 13:15:37 -0500

    ok, that worked

--- browserlog.js
+++ browserlog.js
@@ -28,16 +28,13 @@ kv.get('log', function (err, file) {
       open(msg).then(opened => {
         if (opened) {
           newarray.push(opened)
-          console.log(opened)
         }
       })
     })
 
-    newarray.sort((a,b) => a.timestamp - b.timestamp)
-    console.log(arraystore.length)
     setTimeout(function () {
+      newarray.sort((a,b) => a.timestamp - b.timestamp)
       arraystore = newarray
-      console.log(arraystore.length)
     }, 5000)
     save()
   }
