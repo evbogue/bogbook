8a18ebf5c7b218474ac258aa37f265f2a0bf3cf4
Ev Bogue <ev@evbogue.com>
2023-01-14 15:53:02 -0600

    maybe the serve module will work on deno deploy

--- relay.js
+++ relay.js
@@ -1,5 +1,6 @@
-import { listenAndServe } from "https://deno.land/std@0.144.0/http/server.ts"
-import { serveDir } from "https://deno.land/std@0.144.0/http/file_server.ts"
+
+import { serve } from 'https://deno.land/std@0.172.0/http/server.ts'
+import { serveDir } from 'https://deno.land/std@0.172.0/http/file_server.ts'
 import { addSocket, rmSocket, gossipMsg } from './gossip.js'
 
 const channel = new BroadcastChannel("")
@@ -9,47 +10,15 @@ channel.onmessage = e => {
   gossipMsg(e.data)
 }
 
-await listenAndServe(":8080", (r) => {
+serve((req) => {
   try {
-    const { socket, response } = Deno.upgradeWebSocket(r)
+    const { socket, response } = Deno.upgradeWebSocket(req)
     addSocket(socket)
     socket.onmessage = channel.onmessage
     socket.onclose = _ => rmSocket(socket)
     return response
   } catch {
-    return serveDir(r, {fsRoot: '', showDirListing: true, quiet: true})
+    return serveDir(req, {fsRoot: '', showDirListing: true, quiet: true})
   }
-})
-
-
-//import { serveDir } from "https://deno.land/std@0.170.0/http/file_server.ts"
-//import { addSocket, rmSocket, gossipMsg } from './gossip.js'
-//
-//const server = Deno.listen({ port: 8080 })
-//
-//for await (const conn of server) {
-//  handleHttp(conn).catch(console.error)
-//}
-//
-//const channel = new BroadcastChannel("")
-//
-//channel.onmessage = e => {
-//  (e.target != channel) && channel.postMessage(e.data)
-//  gossipMsg(e.data)
-//}
-//
-//async function handleHttp(conn) {
-//  const httpConn = Deno.serveHttp(conn)
-//  for await (const e of httpConn) {
-//    try {
-//      const { socket, response } = Deno.upgradeWebSocket(e.request)
-//      addSocket(socket)
-//      socket.onmessage = channel.onmessage
-//      socket.onclose = _ => rmSocket(socket)
-//      e.respondWith(response)
-//    } catch {
-//      e.respondWith(serveDir(e.request, {fsRoot: '', showDirListing: true, quiet: true}))
-//    }
-//  }
-//}
+}, { port: 8080 })
 
--- testingrelay.js
+++ testingrelay.js
@@ -1,28 +1,15 @@
-import { serveDir } from "https://deno.land/std@0.170.0/http/file_server.ts"
+import { serve } from 'https://deno.land/std@0.172.0/http/server.ts'
+import { serveDir } from 'https://deno.land/std@0.172.0/http/file_server.ts'
 import { addSocket, rmSocket, gossipMsg } from './gossip.js'
 
-const port = 8080
-
-const server = Deno.listen({ port })
-
-console.log('Listening at http://localhost:' + port + '/')
-
-for await (const conn of server) {
-  handleHttp(conn).catch(console.error)
-}
-
-async function handleHttp(conn) {
-  const httpConn = Deno.serveHttp(conn)
-  for await (const e of httpConn) {
-    try {
-      const { socket, response } = Deno.upgradeWebSocket(e.request)
-      addSocket(socket)
-      socket.onmessage = ev => { gossipMsg(ev.data) }
-      socket.onclose = _ => rmSocket(socket)
-      e.respondWith(response)
-    } catch {
-      e.respondWith(serveDir(e.request, {fsRoot: '', showDirListing: true, quiet: true}))
-    }
+serve((req) => {
+  try {
+    const { socket, response } = Deno.upgradeWebSocket(req)
+    addSocket(socket)
+    socket.onmessage = ev => gossipMsg(ev.data) 
+    socket.onclose = _ => rmSocket(socket) 
+    return response
+  } catch {
+    return serveDir(req, {fsRoot: '', showDirListing: true, quiet: true})
   }
-}
-
+}, { port: 8080 })
