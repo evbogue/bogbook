ae2418072ee2858e7e9bd52b8e32b62c7e049b76
Ev Bogue <ev@evbogue.com>
2023-02-23 12:44:20 -0600

    adder did not need to be an async function

--- adder.js
+++ adder.js
@@ -1,7 +1,6 @@
 import { render } from './render.js' 
-import { open } from './sbog.js'
 
-async function addPosts (posts, div) {
+function addPosts (posts, div) {
   posts.forEach(msg => {
     render(msg).then(rendered => {
       div.appendChild(rendered)
@@ -13,20 +12,22 @@ export function adder (log, src, div) {
   if (log && log[0]) {
     let index = 0
 
-    var reverse = log.slice().reverse()
-    var posts = reverse.slice(index, index + 25)
-    addPosts(posts, div).then(done => {
-      index = index + 25
-      window.onscroll = function (ev) {
-        if (
-          ((window.innerHeight + window.scrollY) >= document.body.scrollHeight - 1000)
-          && window.location.hash.substring(1) === src
-        ) {
-          posts = reverse.slice(index, index + 25)
-          index = index + 25
-          addPosts(posts, div)
-        }
+    const reverse = log.slice().reverse()
+    let posts = reverse.slice(index, index + 25)
+
+    addPosts(posts, div)
+    index = index + 25
+
+    window.onscroll = () => {
+      if (
+        ((window.innerHeight + window.scrollY) >= document.body.scrollHeight - 1000)
+        && window.location.hash.substring(1) === src
+      ) {
+        posts = reverse.slice(index, index + 25)
+        index = index + 25
+        addPosts(posts, div)
       }
-    })
+    }
+    
   }
 }
