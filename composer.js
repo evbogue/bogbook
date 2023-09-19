cbd56845916807dfbfca6aa8657cc2bee40640b5
Ev Bogue <ev@evbogue.com>
2023-09-19 14:33:25 -0500

    abstract addressbook into module

--- composer.js
+++ composer.js
@@ -2,55 +2,13 @@ import { h } from './lib/misc.js'
 import { markdown } from './markdown.js'
 import { publish, open } from './sbog.js'
 import { make } from './blob.js'
-import { save, logs } from './log.js'
+import { save } from './log.js'
 import { render } from './render.js'
-import { getName, getImage } from './avatar.js'
 import { blast } from './replicate.js'
 import { cachekv } from './cachekv.js'
-
-function getContacts (textarea, preview, msg) {
-  const span = h('span')
-
-  const button = h('button', {classList: 'btn right', onclick: function () {
-    if (!span.childNodes[1]) {
-      const addrs = h('span')
-      span.appendChild(addrs)
-
-      logs.getFeeds().then(feeds => {
-        feeds.map(feed => {
-          addrs.appendChild(h('button', {classList: 'btn', onclick: function () {
-            cachekv.get('name:' + feed).then(got => {
-              let name = feed.substring(0, 7) + '...'
-              if (got) {
-                name = got
-              }
-              if (textarea.selectionStart || textarea.selectionEnd) {
-                textarea.value = textarea.value.substring(0, textarea.selectionStart)
-                  + ' [' + name + '](' + feed + ') ' +
-                  textarea.value.substring(textarea.selectionEnd, textarea.value.length)
-              } else {
-                textarea.value = textarea.value + ' [' + name + '](' + feed + ')'
-              }
-              setTimeout(function () {
-                preview.innerHTML = marked(textarea.value)
-                cachekv.put('draft:' + msg.hash, textarea.value)
-              }, 50)
-            })
-          }}, [getImage(feed), ' ', getName(feed)]))
-        })
-      })
-    } else {
-      span.removeChild(span.childNodes[1])
-    }
-  }}, ['📇 '])
-
-  span.appendChild(button)
-
-  return span
-}
+import { getContacts } from './contacts.js'
 
 function photoAdder (textarea, preview, msg) {
-
   const uploadButton = h('button', {classList: 'btn right', onclick: function () {
     input.click()
   }}, ['📸 '])
---
+++ contacts.js
@@ -0,0 +1,51 @@
+import { h } from './lib/misc.js'
+import { markdown } from './markdown.js'
+import { logs } from './log.js'
+import { getName, getImage } from './avatar.js'
+import { cachekv } from './cachekv.js'
+
+function contactButton (feed, textarea, preview, msg) {
+  const contact = h('button', {classList: 'right', onclick: function () {
+    cachekv.get('name:' + feed).then(got => {
+      let name = feed.substring(0, 7) + '...'
+      if (got) {
+        name = got
+      }
+      if (textarea.selectionStart || textarea.selectionEnd) {
+        textarea.value = textarea.value.substring(0, textarea.selectionStart)
+          + ' [' + name + '](' + feed + ') ' +
+          textarea.value.substring(textarea.selectionEnd, textarea.value.length)
+      } else {
+        textarea.value = textarea.value + ' [' + name + '](' + feed + ')'
+      }
+      setTimeout(function () {
+        preview.innerHTML = markdown(textarea.value)
+        cachekv.put('draft:' + msg.hash, textarea.value)
+      }, 50)
+    })
+  }}, [getImage(feed), ' ', getName(feed)])
+  return contact
+}
+
+export function getContacts (textarea, preview, msg) {
+  const span = h('span')
+  const contactsButton = h('button', {classList: 'right', onclick: function () {
+    if (!span.childNodes[1]) {
+      const addrs = h('span')
+      span.appendChild(addrs)
+
+      logs.getFeeds().then(feeds => {
+        feeds.map(feed => {
+          addrs.appendChild(contactButton(feed, textarea, preview, msg))
+        })
+      })
+    } else {
+      span.removeChild(span.childNodes[1])
+    }
+  }}, ['📇 '])
+
+  span.appendChild(contactsButton)
+
+  return span
+}
+
