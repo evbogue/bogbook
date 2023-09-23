f43ff86f9c2cddc40baa839fae0eb4b601d27651
Ev Bogue <ev@evbogue.com>
2023-09-23 15:06:37 -0500

    console.log to try and debug

--- awaitlog.js
+++ awaitlog.js
@@ -2,6 +2,7 @@ import { cachekv } from './cachekv.js'
 
 export const log = async () => {
   const logs = await cachekv.get('log')
+  console.log(logs)
   const arraystore = await cachekv.get('arraystore')
   return logs
 }
