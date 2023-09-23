0c07b46c9aa33a74f2c8ad47292a9fc161a12fef
Ev Bogue <ev@evbogue.com>
2023-09-23 14:43:01 -0500

    try loading log and array store, as we have a race condition on slower machines

--- awaitlog.js
+++ awaitlog.js
@@ -1,6 +1,7 @@
 import { cachekv } from './cachekv.js'
 
 export const log = async () => {
-  const logs = await cachekv.get('arraystore')
+  const logs = await cachekv.get('log')
+  const arraystore = await cachekv.get('arraystore')
   return logs
 }
