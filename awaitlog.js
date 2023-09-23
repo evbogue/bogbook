5c834b637b9525287effbbd1aec93604375564f3
Ev Bogue <ev@evbogue.com>
2023-09-23 15:23:04 -0500

    see if we still have that race condition

--- awaitlog.js
+++ awaitlog.js
@@ -2,7 +2,5 @@ import { cachekv } from './cachekv.js'
 
 export const log = async () => {
   const logs = await cachekv.get('log')
-  console.log(logs)
-  const arraystore = await cachekv.get('arraystore')
   return logs
 }
